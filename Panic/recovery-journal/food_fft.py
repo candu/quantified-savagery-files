62a15f7fa3e5abdad40987b2a8e2b9430e7604f3
Evan Savage <savage.evan@gmail.com>
2012-10-10 15:32:13 -0400

    Better FFT plot, savefig() instead of show()
    so that I can upload the plot images to Picasa :)

--- Panic/recovery-journal/food_fft.py
+++ Panic/recovery-journal/food_fft.py
@@ -1,21 +1,34 @@
 import csv
+import math
 import numpy
 import scipy.fftpack
 import pylab
 
 def getFFTFrequencyStrengths(X, dt=1.0):
   F = scipy.fftpack.fft(X)
-  Q = scipy.fftpack.fftfreq(F.size)
-  return map(numpy.abs, F), Q
+  N = F.size
+  Q = scipy.fftpack.fftfreq(N)
+  return map(numpy.abs, F / math.sqrt(N)), Q
+
+def setup_plot(xlabel, ylabel, title):
+  pylab.xlabel(xlabel)
+  pylab.ylabel(ylabel)
+  pylab.title(title)
+  pylab.grid(True, color=LIGHT_GREY, linestyle='-')
 
 with open('food-diary', 'r') as f:
   reader = csv.DictReader(f)
   L = list(reader)[:31]
 
+RED = '#ff0000'
+LIGHT_GREY = '#d7d7d7'
+
 X = map(lambda row: int(row['alcohol']), L)
 F, Q = getFFTFrequencyStrengths(X)
-pylab.plot(Q, F, 'o')
-pylab.show()
+pylab.plot(Q, F, color=RED, marker='o', linestyle='None')
+setup_plot('Frequency (1/days)', 'Amplitude', 'Frequency Strengths')
+pylab.savefig('fft-frequencies.png')
+pylab.close()
 
-pylab.plot(1.0 / Q, F, 'o')
-pylab.show()
+for f, p in sorted(zip(F, 1.0 / Q))[-5:]:
+  print '[%.2f days] %.4f' % (p, f)
